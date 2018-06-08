e6de36f0253b7805bc9e3574ae4289563c93187b
IRONFELIX <sergo@shvili.me>
2018-06-08 20:00:23 +0300

    Add submenu for registered user. Added header in base template

--- systemapps/accounts/urls.py
+++ systemapps/accounts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, re_path
 from django.contrib.auth import views as auth_views
 
 accounts_urlpatterns = [
-    # re_path('^login/$', auth_views.login, name='login'),
+    re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
 ]
