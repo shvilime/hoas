4737a62328f3ca9555f5f4203eac8405851de01f
Sergo@Home <sergo@shvili.me>
2018-09-24 18:11:42 +0300

    Template for loginsignup form

--- systemapps/accounts/views.py
+++ systemapps/accounts/views.py
@@ -5,11 +5,15 @@ from systemapps.accounts.forms import SignupForm, LoginForm
 
 def loginsignup(request):
     if request.method == 'POST':
-        loginform = LoginForm(request.POST)
-        signupform = SignupForm(request.POST)
-        if loginform.is_valid():
-            return
+        login_form = LoginForm(request.POST)
+        signup_form = SignupForm(request.POST)
+        if 'login-form-submit' in request.POST:
+            if login_form.is_valid():
+                return
+        if 'signup-form-submit' in request.POST:
+            if signup_form.is_valid():
+                return
     else:
-        loginform = LoginForm()
-        signupform = SignupForm()
-    return render(request, 'loginsignup.html', {'form': loginform})
+        login_form = LoginForm()
+        signup_form = SignupForm()
+    return render(request, 'loginsignup.html', {'login_form': login_form, 'signup_form': signup_form})
